8a5f80b8755ac0c41381cd9ce24fd1839d03fb95
ChickenKaraage <ChickenKaraage@users.noreply.github.com>
2025-03-17 05:23:48 +0000

    deploy: ChickenKaraage/ChineseHistory@cdaa3b7687f165cb8e3403b1730fa753c261c65e

---
+++ assets/index-O7xswjg0.js
@@ -0,0 +1,1 @@
+import{w as t,B as e}from"./root-BOv8Nkl7.js";import{j as o,N as r}from"./chunk-K6CSEXPM-CZM0g_7M.js";const i=t(function(){return o.jsx(r,{to:`${e}/en`,replace:!0})});export{i as default};
