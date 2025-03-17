6122f57a58c81c536dd416612196c6d57bade122
ChickenKaraage <ChickenKaraage@users.noreply.github.com>
2025-03-17 00:32:09 +0000

    deploy: ChickenKaraage/ChineseHistory@5a99bce4558fdeaf657f6c6999817269761f410d

---
+++ assets/index-c3IYou2q.js
@@ -0,0 +1,1 @@
+import{w as t,B as e}from"./root-BKfYRMLZ.js";import{j as o,N as r}from"./chunk-K6CSEXPM-CZM0g_7M.js";const i=t(function(){return o.jsx(r,{to:`${e}/en`,replace:!0})});export{i as default};
